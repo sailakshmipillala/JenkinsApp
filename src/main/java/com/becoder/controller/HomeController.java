b44903c630ff19605fbeb0bbff4423be419e7c31
nuthankumarsahukara <142878837+nuthankumarsahukara@users.noreply.github.com>
2024-02-17 19:07:41 +0530

    Update HomeController.java

--- src/main/java/com/becoder/controller/HomeController.java
+++ src/main/java/com/becoder/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.becoder.entity.Employee;
 import com.becoder.service.EmpService;
